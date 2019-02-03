455af94d6de1123cbef3424ebab8e9b7b035ff66
James Askew <jamesaskew@outlook.com>
2019-02-03 12:15:23 +0000

    updated api endpoint for produciton

--- Flatbond.Web/src/api/api.js
+++ Flatbond.Web/src/api/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const getConfig = async () => {
-  return axios.get("/api/config/1");
+  return axios.get("https://flatbondapi.azurewebsites.net/api/config/1");
 };
 
 const postFlatbond = async flatbondDto => {
-  return axios.post("api/flatbond", flatbondDto);
+  return axios.post(
+    "https://flatbondapi.azurewebsites.net/api/flatbond",
+    flatbondDto
+  );
 };
 
 export { getConfig, postFlatbond };
